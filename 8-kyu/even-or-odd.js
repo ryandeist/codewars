e2438dc603ce619f68a2e74ba8a3a4c5d97f9838
ryandeist <ryandeist@gmail.com>
2024-09-19 13:57:58 -0400

    even or odd codewars kata

---
+++ 8-kyu/even-or-odd.js
@@ -0,0 +1,15 @@
+// Description:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    };
+  };
+
+
+console.log(evenOrOdd(5));
+console.log(evenOrOdd(4));
+
